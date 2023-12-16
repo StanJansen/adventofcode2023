9bc372216c0e948de62afeeafdb6ad41247ec6a1
Stan Jansen <stanjan@live.nl>
2023-12-16 12:47:08 +0100

    Day 16 step 2 sped up using wait groups

--- day16/step2/step2.go
+++ day16/step2/step2.go
@@ -2,21 +2,43 @@ package step2
 
 import (
 	"bytes"
+	"sync"
 
 	"github.com/StanJansen/adventofcode2023/day16"
 )
 
 func Solve(input string) int {
 	tiles := day16.Tiles(bytes.Fields([]byte(input)))
-	highest := 0
+	sums := make([]int, len(tiles)*2+len(tiles[0])*2)
+	wg := sync.WaitGroup{}
+	wg.Add(len(sums))
+
+	count := func(x, y, i int, d day16.Direction) {
+		sums[i] = tiles.Energized(int8(x), int8(y), d)
+		wg.Done()
+	}
+
+	i := 0
 	for y := range tiles {
-		highest = max(highest, tiles.Energized(0, int8(y), day16.RIGHT))
-		highest = max(highest, tiles.Energized(int8(len(tiles[0])-1), int8(y), day16.LEFT))
+		go count(0, y, i, day16.RIGHT)
+		i++
+		go count(len(tiles[y])-1, y, i, day16.LEFT)
+		i++
 	}
 	for x := range tiles[0] {
-		highest = max(highest, tiles.Energized(int8(x), 0, day16.DOWN))
-		highest = max(highest, tiles.Energized(int8(x), int8(len(tiles)-1), day16.UP))
+		go count(x, 0, i, day16.DOWN)
+		i++
+		go count(x, len(tiles)-1, i, day16.UP)
+		i++
 	}
+
+	wg.Wait()
+
+	highest := 0
+	for _, sum := range sums {
+		highest = max(highest, sum)
+	}
+
 	return highest
 }
 
