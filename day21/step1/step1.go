d672a3196c8083128a1b1292d732625af033b323
Stan Jansen <s.jansen@btcdirect.eu>
2023-12-21 21:56:55 +0100

    Last day 21 optimalizations...

--- day21/step1/step1.go
+++ day21/step1/step1.go
@@ -9,8 +9,9 @@ const STEPS = 64
 
 type Garden struct {
 	Grid        [][]byte
-	StartPoints [][2]int
-	Seen        map[[2]int]bool
+	StartPoints [][2]byte
+	Seen        map[[2]byte]bool
+	Size        byte
 }
 
 func Solve(input string) int {
@@ -29,23 +30,24 @@ func createGarden(input string) Garden {
 
 	return Garden{
 		Grid:        grid,
-		StartPoints: [][2]int{{startX, (startIdx - startX) / len(grid[0])}},
+		StartPoints: [][2]byte{{byte(startX), byte((startIdx - startX) / len(grid[0]))}},
+		Size:        byte(len(grid) - 1),
 	}
 }
 
 func (g *Garden) TakeStep() {
-	sp := [][2]int{}
-	g.Seen = make(map[[2]int]bool, len(g.StartPoints)*4)
+	sp := [][2]byte{}
+	g.Seen = make(map[[2]byte]bool, len(g.StartPoints)*4)
 
 	for _, p := range g.StartPoints {
-		newPoints := [][2]int{
+		newPoints := [][2]byte{
 			{p[0] - 1, p[1]},
 			{p[0] + 1, p[1]},
 			{p[0], p[1] - 1},
 			{p[0], p[1] + 1},
 		}
 		for _, newPoint := range newPoints {
-			if newPoint[0] < 0 || newPoint[0] > len(g.Grid[0])-1 || newPoint[1] < 0 || newPoint[1] > len(g.Grid)-1 {
+			if newPoint[0] < 0 || newPoint[0] > g.Size || newPoint[1] < 0 || newPoint[1] > g.Size {
 				continue
 			}
 			if g.Grid[newPoint[1]][newPoint[0]] == '#' {
--- day21/step2/step2.go
+++ day21/step2/step2.go
@@ -20,17 +20,11 @@ func Solve(input string) int {
 	size := int(garden.Size)
 	subSteps := size - STEPS%(size+1)
 
-	for i := 0; i < subSteps; i++ {
-		garden.TakeStep()
-	}
+	garden.TakeSteps(subSteps)
 	a := len(garden.StartPoints)
-	for i := 0; i <= size; i++ {
-		garden.TakeStep()
-	}
+	garden.TakeSteps(size)
 	b := len(garden.StartPoints)
-	for i := 0; i <= size; i++ {
-		garden.TakeStep()
-	}
+	garden.TakeSteps(size)
 	c := len(garden.StartPoints)
 
 	m := (STEPS - subSteps) / int(garden.Size+1)
@@ -49,41 +43,44 @@ func createGarden(input string) Garden {
 	}
 }
 
-func (g *Garden) TakeStep() {
-	sp := [][4]byte{}
-	g.Seen = make(map[[4]byte]bool, len(g.StartPoints)*4)
+func (g *Garden) TakeSteps(steps int) {
+	for i := 0; i < steps; i++ {
+		sp := make([][4]byte, 0, len(g.StartPoints)*4)
+		g.Seen = make(map[[4]byte]bool, len(g.StartPoints)*4)
 
-	for _, p := range g.StartPoints {
-		newPoints := [4][4]byte{
-			{p[0] - 1, p[1], p[2], p[3]},
-			{p[0] + 1, p[1], p[2], p[3]},
-			{p[0], p[1] - 1, p[2], p[3]},
-			{p[0], p[1] + 1, p[2], p[3]},
+		for _, p := range g.StartPoints {
+			newPoints := [4][4]byte{
+				{p[0] - 1, p[1], p[2], p[3]},
+				{p[0] + 1, p[1], p[2], p[3]},
+				{p[0], p[1] - 1, p[2], p[3]},
+				{p[0], p[1] + 1, p[2], p[3]},
+			}
+			for _, newPoint := range newPoints {
+				if newPoint[0] == MAX_INT {
+					newPoint[0] = g.Size
+					newPoint[2]--
+				} else if newPoint[0] > g.Size {
+					newPoint[0] = 0
+					newPoint[2]++
+				}
+				if newPoint[1] == MAX_INT {
+					newPoint[1] = g.Size
+					newPoint[3]--
+				} else if newPoint[1] > g.Size {
+					newPoint[1] = 0
+					newPoint[3]++
+				}
+				if g.Grid[newPoint[1]][newPoint[0]] == '#' {
+					continue
+				}
+				if _, ok := g.Seen[newPoint]; ok {
+					continue
+				}
+				sp = append(sp, newPoint)
+				g.Seen[newPoint] = true
+			}
 		}
-		for _, newPoint := range newPoints {
-			if newPoint[0] == MAX_INT {
-				newPoint[0] = g.Size
-				newPoint[2]--
-			} else if newPoint[0] > g.Size {
-				newPoint[0] = 0
-				newPoint[2]++
-			} else if newPoint[1] == MAX_INT {
-				newPoint[1] = g.Size
-				newPoint[3]--
-			} else if newPoint[1] > g.Size {
-				newPoint[1] = 0
-				newPoint[3]++
-			}
-			if g.Grid[newPoint[1]][newPoint[0]] == '#' {
-				continue
-			}
-			if _, ok := g.Seen[newPoint]; ok {
-				continue
-			}
-			sp = append(sp, newPoint)
-			g.Seen[newPoint] = true
-		}
+
+		g.StartPoints = sp
 	}
-
-	g.StartPoints = sp
 }
